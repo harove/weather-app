7ffcd535aa1742f2b4ca59f32565541c4cb0e94e
edgar <harove@gmail.com>
2019-11-20 20:15:04 -0300

    temp, minTemp, maxTemp, weatherDesc ok

--- src/App.js
+++ src/App.js
@@ -12,32 +12,57 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
+      city: undefined,
+      country: undefined,
+      temp: undefined,
+      temp_min: undefined,
+      temp_max: undefined,
+      weatherDesc: undefined,
 
     };
     this.getWeather();
   }
 
-
-   
+  fahrenheitInCelsius = f => Math.floor(f-273.15); 
 
   getWeather = async () => {
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey}`);
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Valparaiso,cl&appid=${apiKey}`);
+    console.log(api_call);
     const response = await api_call.json();
     console.log(response);
+    const {temp,temp_min,temp_max} = response.main;
+    this.setState({
+      city: response.name,
+      temp: this.fahrenheitInCelsius(temp),
+      temp_min: this.fahrenheitInCelsius(temp_min),
+      temp_max: this.fahrenheitInCelsius(temp_max),
+      weatherDesc: response.weather[0].main,
+    });
   }
+
+
+ 
 
 
 
   render() {
+
+    const {temp,temp_min,temp_max} = this.state;
+
     return (
+
       <div className="App">
         <Container>
           <Col lg="6">
             <Card>
               <h1>Weather App</h1>
-              <h1>Santiago</h1>
-              <Weather />
-
+                <Weather
+                  city = {this.state.city}
+                  temp = {temp}
+                  minTemp = {temp_min}
+                  maxTemp = {temp_max}
+                  weatherDesc = {this.state.weatherDesc}
+                />
             </Card>
           </Col>
         </Container>
--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -14,19 +14,16 @@ import './Weather.css';
 
 
 
-const Weather = (props) => {
-
-    //const apiKey = 'd47bc5e52c9b46f24443d3c321a13ddc';
-    //api.openweathermap.org/data/2.5/weather?q={city name}
-
+const Weather = ({city,temp,minTemp,maxTemp,weatherDesc}) => {
     return (
         <div className="weather">
+            <h1>{city}</h1>
             <h4>
                 <i className = "wi wi-day-sunny display-1" > </i>    
             </h4> 
-            <h1 className = "py-2">25&deg;</h1>
-            {minmaxTemp(19,24)}  
-            <h4 className="py-3">slow rain</h4>
+    <h1 className = "py-2">{temp}&deg;</h1>
+            {minmaxTemp(minTemp,maxTemp)}  
+    <h4 className="py-3">{weatherDesc}</h4>
         </div>
     );
 };
@@ -40,6 +37,9 @@ const minmaxTemp = (min,max) => {
        
     );
 }
+
+ 
+
 
 
 export default Weather;
